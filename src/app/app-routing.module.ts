fbb976df8c5dd7776a040b49a861253a0960a229
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-10-22 16:58:07 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SynonymComponent } from './synonym/synonym.component';
 import { SimilarComponent } from './similar/similar.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/synonyms', pathMatch: 'full' },
+  { path: '', component:SynonymComponent },
   { path: 'synonyms', component: SynonymComponent },
   { path: 'similar', component: SimilarComponent },
 ];
